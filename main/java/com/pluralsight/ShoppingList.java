8b33afb445624478199ca5228a9e45f5db939871
Richie <richiesyin@icloud.com>
2025-09-22 15:27:40 -0700

    First Commit

---
+++ main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Milk");
+        System.out.println("Water");
+        System.out.println("Chocolate");
+        System.out.println("Waffles");
+        System.out.println("Vegetables");
+        System.out.println("Meat");
+        System.out.println("Cheese");
+        System.out.println("Soda");
+        System.out.println("Eggs");
+        System.out.println("Coffee");
+    }
+}
