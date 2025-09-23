e6ea23ffbad0cbda1de6bfe955356d79a6fc9be8
Richie <richiesyin@icloud.com>
2025-09-23 15:17:29 -0700

    Game Themed Java Excercises

---
+++ main/java/com/pluralsight/GameThemedJavaExcercises.java
@@ -0,0 +1,81 @@
+package com.pluralsight;
+
+public class GameThemedJavaExcercises {
+    public static void main(String[] args) {
+//        ***************   EASY  ***************
+//        Counting Coins A player collects 137 gold coins. Each bag can hold 20 coins. 👉 How many full bags can the player fill, and how many coins are left over?
+//        int playerCoins = 137;
+//        int bag = 20;
+//
+//        System.out.println("Full bags:" + playerCoins/bag);
+//        System.out.println("Remainder:" + playerCoins%bag);
+
+//        Potion Mixing A potion requires 3 red herbs and 2 blue herbs. The player has 17 red herbs and 11 blue herbs. 👉 How many potions can they make?
+//            int playerRedHerbs = 17;
+//            int playerBlueHerbs = 11;
+//            int requiredRed = 3;
+//            int requiredBlue = 2;
+//            int totalRed = playerRedHerbs/requiredRed;
+//            int totalBlue = playerBlueHerbs/requiredBlue;
+//            int potion = 0;
+//
+//            if (totalRed > totalBlue) {
+//                potion = totalRed;
+//            } else {
+//                potion = totalBlue;
+//            }
+//
+//            System.out.println(potion);
+
+//        Simulate rolling a 6-sided die (int dieRoll = 4;) and add it to the player’s base damage (10). 👉 Print the total damage.
+//            int baseDmg = 10;
+//            int roll1 = 1;
+//            int roll2 = 2;
+//            int roll3 = 3;
+//            int roll4 = 4;
+//            int roll5 = 5;
+//            int roll6 = 6;
+//
+//            System.out.println(roll4 + baseDmg);
+//            System.out.println(roll4 + baseDmg + roll2);
+
+//        Character Health A monster hits the hero for 12 points of damage. The hero starts with 100 HP. 👉 Print the hero’s HP after the attack.
+//            int charHealth = 100;
+//            int monsterHit = -12;
+//
+//            System.out.println(charHealth + monsterHit);
+//            System.out.println(charHealth + monsterHit + monsterHit);
+
+//        Mana Recharge Mana starts at 45. Each turn it regenerates by 10. 👉 Show the mana after 1, 2, and 3 turns (without loops, just calculation).
+//            int mana = 45;
+//            int turn = 10;
+//
+//            System.out.println(mana + turn);
+//            System.out.println(mana + turn + turn);
+//            System.out.println(mana + turn + turn + turn);
+//        ***************   MEDIUM  ***************
+//          Ammo Reloading A player has 53 bullets, and each magazine holds 7. 👉 Print how many full magazines they have and how many bullets are left.
+//            int bullets = 53;
+//            int magazine = 7;
+//
+//            System.out.println("Full Magazines:" + bullets/magazine);
+//            System.out.println("Remainder:" + bullets%magazine);
+
+//          The player has 1180 XP. Each level requires 500 XP. 👉 Print the current level and leftover XP.
+//            int playerXP = 1180;
+//            int levelUp = 500;
+//
+//            System.out.println("Level: " + playerXP/levelUp);
+//            System.out.println("Leftover: " + playerXP%levelUp);
+//          Critical Hit Counter Start with int critCount = 0;. 👉 Use ++ to simulate landing 3 critical hits in a row and print after each hit.
+//            int critCount = 0;
+//
+//            System.out.println(++critCount);
+//            System.out.println(++critCount);
+//            System.out.println(++critCount);
+
+//          Speed Boost A car’s speed is 150 km/h. It picks up a boost of +20, then another +30. 👉 Print the final speed, then reduce it by 50 when the boost runs out.
+//            https://github.com/BrightBoost/learningjava/blob/main/exercises/operatorExercise.md
+    }
+}
+
