040ef9f11b777aa2c0c6c1b590f783235ced9295
Richie <richiesyin@icloud.com>
2025-09-23 11:59:30 -0700

    Java Variables practice

---
+++ main/java/com/pluralsight/JavaVariables.java
@@ -0,0 +1,27 @@
+package com.pluralsight;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+        int vin = 4569761;
+        String make = "Ford Explorer";
+        String color = "Green";
+        boolean towing = true;
+        int odometer = 123578;
+        double price = 182952.99;
+        char rating = 'A';
+        String phoneNumber = "+1 (206)-468-7044";
+        int ssn = 2062062066;
+        int zip = 98168;
+
+        System.out.println(vin);
+        System.out.println(make);
+        System.out.println(color);
+        System.out.println(towing);
+        System.out.println(odometer);
+        System.out.println(price);
+        System.out.println(rating);
+        System.out.println(phoneNumber);
+        System.out.println(ssn);
+        System.out.println(zip);
+    }
+}
