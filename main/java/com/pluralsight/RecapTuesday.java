2fc69995fb6d25c2dd3c35e078c0fc416b2cf619
Richie <richiesyin@icloud.com>
2025-09-24 10:39:49 -0700

    RecapTuesday

---
+++ main/java/com/pluralsight/RecapTuesday.java
@@ -0,0 +1,46 @@
+package com.pluralsight;
+
+public class RecapTuesday {
+    public static void main(String[] args) {
+        //1
+//        double age = 12;
+//
+//        System.out.println(age);
+//        System.out.println(age * 2);
+        //2
+//        String name = "Richie";
+//        int favNum = 69;
+//
+//        System.out.println("My name is " + name + " and my favorite number is " + favNum);
+
+        //3
+//        int ranNum = (int) (Math.random() * 10);
+//        System.out.println("Random number:" + ranNum);
+//        System.out.println("After adding 5: " + (ranNum + 5));
+        //4
+//        int hours = 27;
+//        System.out.println(hours + " hours later -> " + (hours % 24) + " o'clock");
+        //5
+//        int hours = 0;
+//        int leftovers = 0;
+//        int minutes = 260;
+//
+//        if(minutes >= 60) {
+//            hours = minutes/60;
+//            leftovers = minutes%60;
+//        }
+//
+//        System.out.println(minutes + " minutes = " + hours + " hours and " + leftovers + " minutes");
+        //6
+//        int numHours = 3;
+//        int numMinutes = 23;
+//        int minutesToAdd = 183;
+//        int newHours = minutesToAdd/60;
+//        int newMins = minutesToAdd%60;
+//
+//
+//        System.out.println("Start time: " + numHours + ":" + numMinutes);
+//        System.out.println("Minutes to add: " + minutesToAdd);
+//        System.out.println("Final time: " + (numHours + newHours) + ":" + (numMinutes + newMins));
+    }
+}
